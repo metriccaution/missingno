852b208c0a5355362c3bf8c54284e63044d356b0
metriccaution <xang@hotmail.co.uk>
2016-11-06 17:59:40 +0000

    Removing test template

--- src/main/java/com/github/metriccaution/missingno/listing/ContentListingRouter.java
+++ src/main/java/com/github/metriccaution/missingno/listing/ContentListingRouter.java
@@ -1,26 +1,11 @@
 package com.github.metriccaution.missingno.listing;
 
-import java.util.HashMap;
-
 import com.github.metriccaution.missingno.Router;
-import com.google.common.collect.Maps;
-
-import spark.ModelAndView;
-import spark.template.jade.JadeTemplateEngine;
 
 public class ContentListingRouter extends Router {
 
 	public ContentListingRouter(final String base) {
 		super(base);
-
-		// TODO - Templating
-		get("/*", (req, res) -> {
-			final HashMap<Object, Object> data = Maps.newHashMap();
-
-			data.put("context", "Context Data");
-
-			return new ModelAndView(data, "no-context");
-		}, new JadeTemplateEngine());
 	}
 
 }
